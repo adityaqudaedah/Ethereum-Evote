5a3c5b32ddb8cdb3bd67e84e9f6a56147d17f4ca
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2022-05-26 05:54:04 +0700

    styling

--- client/src/Pages/Voter/components/unvoted.jsx
+++ client/src/Pages/Voter/components/unvoted.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import { Heading } from '@chakra-ui/react'
+import React from "react";
+import { Heading, Flex, Image, Box } from "@chakra-ui/react";
 
 export const Unvoted = () => {
   return (
-    <Heading size="lg" color="red.200">Anda Belum Diberi Hak Akses Untuk Melakukan Voting. Silahkan Hubungi Admin</Heading>
-  )
-}
+    <Flex mt="2rem" flexDirection="column" alignItems="center">
+      <Box boxSize="sm">
+        <Image src="../../../../assets/permission.svg" />
+      </Box>
+
+      <Heading size="lg" color="blackAlpha.500">
+        Anda belum diberi hak akses untuk melakukan voting
+      </Heading>
+    </Flex>
+  );
+};
